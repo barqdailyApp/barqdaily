cd043486f977cee74428d1619bde37e0bbe08b10
Omar Wael <omarwael@Omars-MacBook-Air.local>
2024-09-24 12:53:21 +0300

    Translation commit

--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -1,18 +1,104 @@
-import { Box } from "@mui/material";
+"use client";
+import { Box, Container, IconButton } from "@mui/material";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-import { HEADER } from "@/layouts/config-layout";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { useLocale } from "next-intl";
+import { localesSettings, LocaleType } from "@/i18n/config-locale";
+
 
 // ----------------------------------------------------------------------
-
-export default function HomeView() {
-  return (
+interface Props {
+  slides : {
+    caption:string,
+    image:string
+  }[]
+}
+export default function HomeView({slides}:Props) {
+  const locale = useLocale();
+  const dir =localesSettings[locale as LocaleType].dir
+  return(
     <Box
-      sx={{
-        pt: { xs: `${HEADER.H_MOBILE}px`, md: `${HEADER.H_DESKTOP}px` },
-        textAlign: "center",
-      }}
-    >
-      Home Page
-    </Box>
-  );
+    sx={(theme)=>({
+      textAlign: 'center',
+      pb: 0,
+      background:theme.palette.background.neutral,
+     
+    })}
+  >
+    <Container style={{marginTop:1}}>
+      <Box  sx={{ width: '100%', position: 'relative', px:0.5 ,py:6 }}>
+        <Swiper
+          
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          loop
+          autoplay={{ delay: 3000 }}
+          pagination={{ clickable: true }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          className="mySwiper"
+        >
+          {slides?.map((item, index) => (
+            <SwiperSlide key={index}>
+              <img className="responsive-banar" src={item.image} alt="banner" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <IconButton
+          sx={(theme)=>({
+            position: 'absolute',
+            left: 0,
+            top: '50%',
+            transform: `translateY(-50%)`,
+            backgroundColor:theme.customShadows.card,
+            zIndex:10
+            
+            
+            
+    })}
+          className="swiper-button-next"
+        >
+          <ArrowBackIosNewIcon
+          sx={{
+            fontSize: '30px',
+            transform: dir === 'rtl' ? 'scaleX(-1)' : undefined
+  
+            
+          }}
+          />
+        </IconButton>
+        <IconButton
+          sx={(theme)=>({
+            position: 'absolute',
+            right:0,
+            top: '50%',
+            transform: `translateY(-50%)`,
+            backgroundColor:theme.customShadows.card,
+            zIndex:10
+                    
+          })}
+          className="swiper-button-prev"
+        >
+          <ArrowForwardIosIcon
+        
+            sx={{
+              fontSize: '30px',
+              transform: dir === 'rtl' ? 'scaleX(-1)' : undefined
+  
+            }}
+          />
+        </IconButton>
+      </Box>
+    </Container>
+  </Box>
+
+);
 }
