6ae1565c378c65315ff3c02e3b6afcbc651157d0
im-beltagy <imbeltagy@gmail.com>
2024-10-01 11:11:59 +0300

    feat: set default locale as ar

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,8 @@ import createMiddleware from "next-intl/middleware";
 
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware({ ...routing, localeDetection: false });
 
 export const config = {
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    "/",
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    "/(ar|en)/:path*",
-
-    // Enable redirects that add missing locales
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    "/((?!_next|_vercel|.*\\..*).*)",
-  ],
+  matcher: ["/", "/(ar|en)/:path*"],
 };
