4fa5e0ce2ed8bfd5d525daf7a5539571f8e70a70
im-beltagy <imbeltagy@gmail.com>
2024-10-02 10:54:30 +0300

    complete merge with development

--- src/app/[locale]/(store-layout)/orders/page.tsx
+++ src/app/[locale]/(store-layout)/orders/page.tsx
@@ -1,11 +1,22 @@
-import { fetchOrders } from "@/actions/order-actions";
+import { endpoints } from "@/utils/endpoints";
+import { getData } from "@/utils/crud-fetch-api";
 
 import OrderView from "@/sections/order/view/order-view";
 
+import { Order } from "@/types/order";
+
+type Response = { data: Order[]; meta: { itemCount: number } };
+
 export default async function Page() {
-  const orders = await fetchOrders();
+  const searchParams = new URLSearchParams({
+    page: "1",
+    limit: "10",
+  });
+  const orders = await getData<Response>(
+    `${endpoints.orders}?${searchParams.toString()}`
+  );
   if ("error" in orders) {
     throw new Error(orders.error);
   }
-  return <OrderView orders={orders.items} />;
+  return <OrderView orders={orders.data.data} />;
 }
