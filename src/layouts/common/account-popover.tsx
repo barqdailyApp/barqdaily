a21f6d1e7c6a2ab3b0b002ff924a54edbb98e809
im-beltagy <imbeltagy@gmail.com>
2024-11-14 00:40:57 +0200

    fix(logout): reload website on logout

--- src/layouts/common/account-popover.tsx
+++ src/layouts/common/account-popover.tsx
@@ -71,6 +71,7 @@ function AccountPopoverContent() {
     try {
       await logout();
       popover.onClose();
+      window.location.reload();
     } catch (error) {
       console.error(error);
       enqueueSnackbar("Unable to logout!", { variant: "error" });
