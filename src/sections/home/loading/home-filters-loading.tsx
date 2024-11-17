e84725693ef92d4589355d59de542c0ad56e2460
im-beltagy <imbeltagy@gmail.com>
2024-11-17 23:23:43 +0200

    style(home-filters-loading): add bottom padding

--- src/sections/home/loading/home-filters-loading.tsx
+++ src/sections/home/loading/home-filters-loading.tsx
@@ -13,7 +13,7 @@ export default function HomeFiltersLoading() {
           />
         ))}
       </Stack>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} sx={{ pb: 3 }}>
         {[...Array(10)].map((_, index) => (
           <Grid item xs={12} sm={6} md={2.4} key={index}>
             <Skeleton variant="rounded" width="100%" height={200} />
