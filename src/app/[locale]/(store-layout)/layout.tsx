943426bb2b11ddb14c176d0d0be08767244ec5b6
im-beltagy <imbeltagy@gmail.com>
2024-11-28 11:38:27 +0200

    chor(product): disable product dialog

--- src/app/[locale]/(store-layout)/layout.tsx
+++ src/app/[locale]/(store-layout)/layout.tsx
@@ -2,19 +2,16 @@ import StoreLayout from "@/layouts/store";
 
 export default async function Layout({
   children,
-  modal,
   initcart,
   noguest,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
   initcart: React.ReactNode;
   noguest: React.ReactNode;
 }) {
   return (
     <StoreLayout>
       {children}
-      {modal}
       {initcart}
       {noguest}
     </StoreLayout>
