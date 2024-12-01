205825f439bdc768dc75b030874f367ab0ef6c43
im-beltagy <imbeltagy@gmail.com>
2024-12-01 15:54:42 +0200

    fix(phone-field): set sliced value to form

--- src/components/hook-form/rhf-text-field.tsx
+++ src/components/hook-form/rhf-text-field.tsx
@@ -16,34 +16,35 @@ export default function RHFTextField({
   maxLength,
   ...other
 }: Props) {
-  const { control } = useFormContext();
+  const { control, setValue } = useFormContext();
 
   return (
     <Controller
       name={name}
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <TextField
-          {...field}
-          fullWidth
-          type={type}
-          value={
-            type === "number" && field.value === 0
-              ? ""
-              : (field.value || "").slice(0, maxLength)
-          }
-          onChange={(event) => {
-            if (type === "number") {
-              field.onChange(Number(event.target.value));
-            } else {
-              field.onChange(event.target.value);
-            }
-          }}
-          error={!!error}
-          helperText={error ? error?.message : helperText}
-          {...other}
-        />
-      )}
+      render={({ field, fieldState: { error } }) => {
+        if (maxLength && field.value?.length > maxLength)
+          setValue(name, field.value.slice(0, maxLength));
+
+        return (
+          <TextField
+            {...field}
+            fullWidth
+            type={type}
+            value={type === "number" && field.value === 0 ? "" : field.value}
+            onChange={(event) => {
+              if (type === "number") {
+                field.onChange(Number(event.target.value));
+              } else {
+                field.onChange(event.target.value);
+              }
+            }}
+            error={!!error}
+            helperText={error ? error?.message : helperText}
+            {...other}
+          />
+        );
+      }}
     />
   );
 }
